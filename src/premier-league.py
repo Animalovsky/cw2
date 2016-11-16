573f5310ff0ad9e36c918d7c3d912e9b448a6f5c
animalovsky <xsirangarx@googlemail.com>
2016-11-16 18:25:51 +0000

    Paths set to html templates.

--- src/premier-league.py
+++ src/premier-league.py
@@ -5,6 +5,26 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
+
+# This is a teams route.
+@app.route('/teams/')
+def teams():
+    return render_template("teams.html")
 	
+# This is a stadiums route.
+@app.route('/stadiums/')
+def stadiums():
+    return render_template("stadiums.html")
+	
+# This is a rankings route.
+@app.route('/rankings/')
+def rankings():
+    return render_template("rankings.html")
+	
+# This is a table route.
+@app.route('/table/')
+def table():
+    return render_template("table.html")
+		
 if __name__  == "__main__":
 	app.run(host='0.0.0.0 ', debug=True)
