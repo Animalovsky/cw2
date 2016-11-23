d9aad11958c88ea5aa72d6fe6da63fd3be27ba20
animalovsky <xsirangarx@googlemail.com>
2016-11-23 18:21:45 +0000

    Teams and single team page have been designed with appropriate styling and JSON data.

--- src/premier-league.py
+++ src/premier-league.py
@@ -1,4 +1,6 @@
 from  flask  import  Flask , render_template, jsonify, json, url_for, redirect, request
+import urllib
+import urlparse
 app = Flask(__name__)
 
 # This is an index route.
@@ -9,13 +11,35 @@ def index():
 # This is a teams route.
 @app.route('/teams/')
 def teams():
-    return render_template("teams.html")
+    json_data=open('static/data.json').read()
+    data= json.loads(json_data)
+    return render_template("teams.html", results=data)
+	
+# This is as single team route.
+@app.route('/<team_name>/')
+def team(team_name):
+    json_data=open('static/data.json').read()
+    team= json.loads(json_data)
+    return render_template("team.html", team=team, team_name=team_name)	
 	
 # This is a stadiums route.
 @app.route('/stadiums/')
 def stadiums():
-    return render_template("stadiums.html")
+    json_data=open('static/stadiums.json').read()
+    stadium= json.loads(json_data)
+    return render_template("stadiums.html", results=stadium)
 	
+# This is a single stadium route.
+@app.route('/<team_name>/<stadium_name>/')
+def stadium(team_name, stadium_name):
+    json_data=open('static/data.json').read()
+    team= json.loads(json_data)
+    json_data=open('static/stadiums.json').read()
+    stadium= json.loads(json_data)
+    return render_template("stadium.html", stadium=stadium, stadium_name=stadium_name, team=team, team_name=team_name)
+		
+
+
 # This is a rankings route.
 @app.route('/rankings/')
 def rankings():
@@ -28,3 +52,4 @@ def table():
 		
 if __name__  == "__main__":
 	app.run(host='0.0.0.0 ', debug=True)
+	
